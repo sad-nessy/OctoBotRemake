2c32735743def68c058cf972d1d1ba3bcbc99f0e
resiakact <168393487+resiakact@users.noreply.github.com>
2024-05-29 01:04:53 +0800

    Create gclist.js

---
+++ cmds/gclist.js
@@ -0,0 +1,73 @@
+module.exports = {
+
+    description: "List of group chats and perform actions",
+
+    role: "admin",
+
+    cooldown: 0,
+
+    execute: async function(api, event, args, commands) {
+
+        if (args[0] === "remove" && args[1]) {
+            const threadID = args[1];
+            api.removeUserFromGroup(api.getCurrentUserID(), threadID, (err) => {
+                if (err) {
+                    api.sendMessage(`Failed to leave group ${threadID}`, event.threadID);
+                } else {
+                    api.sendMessage(`Left group ${threadID} successfully!`, event.threadID);
+                }
+            });
+            return;
+        }
+
+        const inbox = await api.getThreadList(100, null, ['INBOX']);
+
+        const list = [...inbox].filter(group => group.isSubscribed && group.isGroup);
+
+        const listthread = [];
+
+        for (const groupInfo of list) {
+
+            const data = await api.getThreadInfo(groupInfo.threadID);
+
+            listthread.push({
+
+                id: groupInfo.threadID,
+
+                name: groupInfo.name,
+
+                sotv: data.userInfo.length,
+
+            });
+
+        }
+
+        const listbox = listthread.sort((a, b) => b.sotv - a.sotv);
+
+        let msg = '',
+
+            i = 1;
+
+        const groupid = [];
+
+        for (const group of listbox) {
+
+            msg += `${i++}. ${group.name}\n🧩TID: ${group.id}\n🐸Member: ${group.sotv}\n\n`;
+
+            groupid.push(group.id);
+
+        }
+
+        api.sendMessage(msg + 'to remove:\n gclist remove {number}', event.threadID, (err) => {
+
+            if (err) {
+
+                console.error(err);
+
+            }
+
+        });
+
+    }
+
+}; 
